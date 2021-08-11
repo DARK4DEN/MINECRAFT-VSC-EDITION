9fb1c79db0287e58470f82f98ec7ae1d4975c82b
DARK4DEN <88098211+DARK4DEN@users.noreply.github.com>
2021-08-11 15:28:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -132,3 +132,81 @@ if(e.shiftKey && keyPressed == '77')
     }
     
 }
+
+  function up()
+  {
+
+  if(player_y>=0)
+  {
+
+
+ player_y=player_y-block_image_height;
+ console.log ("block image height="+block_image_height);
+ console.log("when up arrow is pressed, x="+player_x+", y="+player_y);
+ canvas.remove(player_object);
+ player_update();
+  }
+
+
+
+
+
+
+  }
+
+  function down()
+  {
+
+     if(player_y<=500)
+    {
+
+
+       player_y=player_y+block_image_height;
+      console.log ("block image height="+block_image_height);
+     console.log("when down arrow is pressed, x="+player_x+", y="+player_y);
+     canvas.remove(player_object);
+     player_update();
+      } 
+
+     }  
+     function left()
+     {
+   
+     if(player_x>0)
+     {
+   
+   
+     player_x=player_x-block_image_width;
+     console.log ("block image width="+block_image_width);
+     console.log("when left arrow is pressed, x="+player_x+", y="+player_y);
+     canvas.remove(player_object);
+     player_update();
+     }
+   
+   
+   
+   
+   
+   
+     }
+
+     function right()
+     {
+   
+     if(player_x<=850)
+     {
+   
+   
+     player_x=player_x+block_image_width;
+     console.log ("block image width="+block_image_width);
+     console.log("when right arrow is pressed, x="+player_x+", y="+player_y);
+     canvas.remove(player_object);
+     player_update();
+     }
+   
+   
+   
+   
+   
+   
+     }
